61e36cddac514756330a23f595aac6074a390001
rinkucool007 <pratap_the_knight@yahoo.com>
2017-03-20 22:26:40 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("This is part of testing");
     }
 }
